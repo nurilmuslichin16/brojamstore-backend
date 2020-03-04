82f267b25cb0c9b9154acc86db63088fd63da944
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-03-04 07:30:14 +0700

    membuat API Checkout

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 
-use App\Request\CheckoutRequest;
+use App\Http\Requests\API\CheckoutRequest;
 
 use App\Models\Product;
 use App\Models\Transaction;
@@ -18,5 +18,20 @@ class CheckoutController extends Controller
     {
         $data = $request->except('transaction_details');
         $data['uuid'] = 'TRX' . mt_rand(10000,99999) . mt_rand(100, 999);
+
+        $transaction = Transaction::create($data);
+
+        foreach ($request->transaction_details as $product) {
+            $details[] = TransactionDetail([
+                'transaction_id' => $transaction->id,
+                'product_id' => $product
+            ]);
+
+            Product::find($product)->decrement('quantity');
+        }
+
+        $transaction->details()->saveMany($details);
+
+        return ResponseFormatter::success($transaction);
     }
 }
